0c5338a0963fef1d0538d86f9866f20915c4a728
Ross Poulton <ross@rossp.org>
2013-06-19 09:36:01 +0000

    Add customer tokens for recurring billing

--- pinpayments/admin.py
+++ pinpayments/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from pinpayments.models import PinTransaction
+from pinpayments.models import PinTransaction, CustomerToken
 
-class PinAdmin(admin.ModelAdmin):
+class PinTransactionAdmin(admin.ModelAdmin):
     list_display = ('date', 'processed', 'succeeded', 'environment', 'amount', 'currency', 'email_address', 'pin_response', 'ip_address', 'transaction_token',)
     search_fields = ('email_address', 'ip_address', 'card_token', 'transaction_token',)
     list_filter = ('processed', 'succeeded', 'environment', 'currency', )
     date_hierarchy = 'date'
+    readonly_fields = ('date', 'description', 'processed', 'succeeded', 'environment', 'amount', 'currency', 'fees', 'email_address', 'pin_response', 'ip_address', 'transaction_token', 'card_token', 'customer_token', 'email_address', 'card_address1', 'card_address2', 'card_city', 'card_state', 'card_postcode', 'card_country', 'card_number', 'card_type', 'pin_response_text')
 
-admin.site.register(PinTransaction, PinAdmin)
+class PinTransactionInline(admin.TabularInline):
+    """ Used to show transactions for a particular customer token, if using 
+    the Customer API. """
+    model = PinTransaction
+    fields = ('date', 'processed', 'succeeded', 'environment', 'amount', 'currency', 'transaction_token', 'ip_address')
+    readonly_fields = fields
+
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('token', 'user', 'environment', 'created','active','card_type','card_number')
+    search_fields = ('token', 'card_number')
+    list_filter = ('environment', 'card_type', 'active')
+    date_hierarchy = 'created'
+    inlines = [PinTransactionInline,]
+    readonly_fields = ('environment', 'token', 'card_type', 'card_number')
+
+admin.site.register(PinTransaction, PinTransactionAdmin)
+admin.site.register(CustomerToken, TokenAdmin)
---
+++ pinpayments/migrations/0004_auto__chg_field_pintransaction_customer_token__add_index_pintransactio.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Renaming column for 'PinTransaction.customer_token' to match new field type.
+        db.rename_column(u'pinpayments_pintransaction', 'customer_token', 'customer_token_id')
+        # Changing field 'PinTransaction.customer_token'
+        db.alter_column(u'pinpayments_pintransaction', 'customer_token_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['pinpayments.CustomerToken'], null=True))
+        # Adding index on 'PinTransaction', fields ['customer_token']
+        db.create_index(u'pinpayments_pintransaction', ['customer_token_id'])
+
+
+    def backwards(self, orm):
+        # Removing index on 'PinTransaction', fields ['customer_token']
+        db.delete_index(u'pinpayments_pintransaction', ['customer_token_id'])
+
+
+        # Renaming column for 'PinTransaction.customer_token' to match new field type.
+        db.rename_column(u'pinpayments_pintransaction', 'customer_token_id', 'customer_token')
+        # Changing field 'PinTransaction.customer_token'
+        db.alter_column(u'pinpayments_pintransaction', 'customer_token', self.gf('django.db.models.fields.CharField')(max_length=40, null=True))
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'pinpayments.customertoken': {
+            'Meta': {'object_name': 'CustomerToken'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'card_number': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'card_type': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'environment': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '25', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'token': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+        },
+        u'pinpayments.pintransaction': {
+            'Meta': {'ordering': "['-date']", 'object_name': 'PinTransaction'},
+            'amount': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '2'}),
+            'card_address1': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'card_address2': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'card_city': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'card_country': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'card_number': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'card_postcode': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'card_state': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'card_token': ('django.db.models.fields.CharField', [], {'max_length': '40', 'null': 'True', 'blank': 'True'}),
+            'card_type': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'currency': ('django.db.models.fields.CharField', [], {'default': "'AUD'", 'max_length': '100'}),
+            'customer_token': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['pinpayments.CustomerToken']", 'null': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'email_address': ('django.db.models.fields.EmailField', [], {'max_length': '100'}),
+            'environment': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '25', 'blank': 'True'}),
+            'fees': ('django.db.models.fields.DecimalField', [], {'default': '0', 'null': 'True', 'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39'}),
+            'pin_response': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'pin_response_text': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'processed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'succeeded': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'transaction_token': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '100', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['pinpayments']
