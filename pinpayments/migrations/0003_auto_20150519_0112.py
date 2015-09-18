45b68ef3d43a935f7e6c0276536bc72b8bac929e
Thomas Randle <thomas@ionata.com.au>
2015-09-18 14:46:02 +1000

    migration fix on create

--- pinpayments/migrations/0003_auto_20150519_0112.py
+++ pinpayments/migrations/0003_auto_20150519_0112.py
@@ -2,18 +2,22 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.db.utils import OperationalError, ProgrammingError
 
 
 def migrate_customer_card_data_to_cardtokens(apps, schema_editor):
     CustomerToken = apps.get_model("pinpayments", "CustomerToken")
     CardToken = apps.get_model("pinpayments", "CardToken")
-    for customer_token in CustomerToken.objects.all():
-        card_token = CardToken()
-        card_token.scheme = customer_token.card_type
-        card_token.display_number = customer_token.card_number
-        card_token.name = customer_token.card_name
-        card_token.save()
-        customer_token.cards.add(card_token)
+    try:
+        for customer_token in CustomerToken.objects.all():
+            card_token = CardToken()
+            card_token.scheme = customer_token.card_type
+            card_token.display_number = customer_token.card_number
+            card_token.name = customer_token.card_name
+            card_token.save()
+            customer_token.cards.add(card_token)
+    except OperationalError, ProgrammingError:
+        pass
 
 
 class Migration(migrations.Migration):
